64b03c17a6aa2679dd8e0ec94325a171f0524b6a
Adrian Carpenter <adriatic.c@gmail.com>
2017-06-08 09:06:56 -0700

    Fix news item cache

---
+++ utils.py
@@ -0,0 +1,38 @@
+#encoding: utf-8
+
+import contextlib
+import traceback
+import os
+
+@contextlib.contextmanager
+def script_directory():
+    '''
+    A context manager which allows you to write blocks of code which run within 
+    this script's directory. The working directory is restored afterward.
+    '''
+    cwd = os.getcwd()
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    try:
+        yield
+    finally:
+        os.chdir(cwd)
+
+
+def raise_hell(f):
+    '''
+    Traceback displaying decorator for callbacks whose exceptions get expunged
+    '''
+
+    import traceback
+    def wrapper(*a, **kw):
+        try: return f(*a, **kw)
+        except Exception:
+            traceback.print_exc(limit=-1)
+            raise
+    return wrapper
+
+
+__all__ = [
+    'raise_hell',
+    'script_directory',
+]
